226f107ae3d4ac08aebe51021fc1f43783bc51f1
Swen Kooij <swen@sectorlabs.ro>
2016-10-21 11:02:56 +0300

    Added a simple setup.py file

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+setup(
+    name='django-localized-fields',
+    version='1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',
+    description='Implementation of localized model fields using PostgreSQL HStore fields.'
+    long_description=README,
+    url='http://sectorlabs.ro/',
+    author='Sector Labs',
+    author_email='open-source@sectorlabs.ro',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: X.Y',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ]
+)
