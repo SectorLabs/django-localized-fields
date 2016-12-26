8f9a504e8df5d82ba991e6d26ca3294b78dac1d3
seroy <seroy@bk.ru>
2016-12-26 06:14:56 +0300

    fixed "Apps aren't loaded yet" exception and direct dependency from django-bleach

--- localized_fields/fields/__init__.py
+++ localized_fields/fields/__init__.py
@@ -1,12 +1,19 @@
 from .localized_field import LocalizedField
 from .localized_value import LocalizedValue
 from .localized_autoslug_field import LocalizedAutoSlugField
-from .localized_bleach_field import LocalizedBleachField
 
 
 __all__ = [
     'LocalizedField',
     'LocalizedValue',
     'LocalizedAutoSlugField',
-    'LocalizedBleachField'
 ]
+
+try:
+    from .localized_bleach_field import LocalizedBleachField
+    __all__ += [
+        'LocalizedBleachField'
+    ]
+except ImportError:
+    pass
+
