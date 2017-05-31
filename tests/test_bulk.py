c75c1764e276d1cbda61e1258eb6e09298bce3ce
Swen Kooij <swen@sectorlabs.ro>
2017-05-31 11:38:53 +0300

    Improve test case for bulk_create

--- tests/test_bulk.py
+++ tests/test_bulk.py
@@ -1,12 +1,8 @@
-import json
-
 from django.db import models
-from django.conf import settings
 from django.test import TestCase
 
 from localized_fields.fields import LocalizedField, LocalizedUniqueSlugField
 
-from .data import get_init_values
 from .fake_model import get_fake_model
 
 
@@ -28,10 +24,22 @@ class LocalizedBulkTestCase(TestCase):
             }
         )
 
-        objects = model.objects.bulk_create([
+        to_create = [
             model(name={'en': 'english name 1', 'ro': 'romanian name 1'}, score=1),
             model(name={'en': 'english name 2', 'ro': 'romanian name 2'}, score=2),
             model(name={'en': 'english name 3', 'ro': 'romanian name 3'}, score=3)
-        ])
+        ]
 
+        model.objects.bulk_create(to_create)
         assert model.objects.all().count() == 3
+
+        for obj in to_create:
+            obj_db = model.objects.filter(
+                name__en=obj.name.en,
+                name__ro=obj.name.ro,
+                score=obj.score
+            ).first()
+
+            assert obj_db
+            assert len(obj_db.slug.en) >= len(obj_db.name.en)
+            assert len(obj_db.slug.ro) >= len(obj_db.name.ro)
