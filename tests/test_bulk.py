6736b3b32de38404447a4839b305dc6b6f7c0d2c
Swen Kooij <swen@sectorlabs.ro>
2017-05-31 11:33:20 +0300

    Simplify test case for bulk_create

--- tests/test_bulk.py
+++ tests/test_bulk.py
@@ -16,27 +16,6 @@ class LocalizedBulkTestCase(TestCase):
 
     @staticmethod
     def test_localized_bulk_insert():
-        """Tests that bulk inserts work properly when using
-        a :see:LocalizedField in the model."""
-
-        model = get_fake_model(
-            'BulkInsertModel',
-            {
-                'name': LocalizedField(),
-                'score': models.IntegerField()
-            }
-        )
-
-        objects = model.objects.bulk_create([
-            model(name={'en': 'english name 1', 'ro': 'romanian name 1'}, score=1),
-            model(name={'en': 'english name 2', 'ro': 'romanian name 2'}, score=2),
-            model(name={'en': 'english name 3', 'ro': 'romanian name 3'}, score=3)
-        ])
-
-        assert model.objects.all().count() == 3
-
-    @staticmethod
-    def test_localized_slug_bulk_insert():
         """Tests whether bulk inserts work properly when using
         a :see:LocalizedUniqueSlugField in the model."""
 
