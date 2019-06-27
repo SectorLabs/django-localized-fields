8494615d05febdcebd93b67d192fd0b82eb0cb61
Swen Kooij <swen@sectorlabs.ro>
2019-06-27 13:28:29 +0300

    Upgrade django-postgres-extra to 1.22

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     author_email='open-source@sectorlabs.ro',
     keywords=['django', 'localized', 'language', 'models', 'fields'],
     install_requires=[
-        'django-postgres-extra>=1.21a16',
+        'django-postgres-extra>=1.22',
         'Django>=1.11',
         'deprecation==2.0.3'
     ],
