afc39745bf83e92309b6e99db2eb7041f88d4f19
Swen Kooij <swen@sectorlabs.ro>
2020-11-30 12:28:03 +0200

    Bump version number to 6.2rc1

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ with open(
 
 setup(
     name="django-localized-fields",
-    version="6.1",
+    version="6.2rc1",
     packages=find_packages(exclude=["tests"]),
     include_package_data=True,
     license="MIT License",
