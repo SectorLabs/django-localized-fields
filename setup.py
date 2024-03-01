4a49081679fcf628288829b24676886c30637d7c
Bogdan Hopulele <bogdan@sectorlabs.ro>
2024-03-01 18:05:03 +0200

    Relax "deprecation" dependency

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ setup(
     install_requires=[
         "Django>=2.0",
         "django-postgres-extra>=2.0,<3.0",
-        "deprecation==2.0.7",
+        "deprecation>=2.0.7",
     ],
     extras_require={
         ':python_version <= "3.6"': ["dataclasses"],
